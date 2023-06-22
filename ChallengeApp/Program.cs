3262e2d0cc49413728ba261d311470ba024e27d6
Kamil Motulewicz <kamil.motulewicz@gmail.com>
2023-06-22 19:16:37 +0200

    Add project files.

---
+++ ChallengeApp/Program.cs
@@ -0,0 +1,36 @@
+﻿string name = "Ewa"; //or "Adam"
+string sex1 = "Kobieta";
+string sex2 = "Mężczyżna";
+int age = 20;
+
+if (name == "Ewa")
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletnia " + sex1);
+    }
+    else if (age > 30)
+    {
+        Console.WriteLine(name + ", lat " + age);
+    }
+    else if (age < 30)
+    {
+        Console.WriteLine(sex1 + " poniżej 30 lat");
+    }
+};
+
+if (name == "Adam")
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni " + sex2);
+    }
+    else if (age > 30)
+    {
+        Console.WriteLine(name + ", lat " + age);
+    }
+    else if (age < 30)
+    {
+        Console.WriteLine(sex2 + " poniżej 30 lat");
+    }
+};
