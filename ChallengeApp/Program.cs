a8f5a55d353bfa6900b8195e924a6484255acdd7
Kamil Motulewicz <kamil.motulewicz@gmail.com>
2023-06-25 18:47:06 +0200

    klasa

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection.Metadata;
 using static System.Formats.Asn1.AsnWriter;
+using ChallengeApp;
 
 User user1 = new User("Ewa","aaa");
 User user2 = new User("Tomek","ppp");
@@ -11,28 +12,4 @@ user1.AddScore(5);
 var result = user1.Result;
 Console.WriteLine(result);
 
-class User
-{
-    private List<int> score = new List<int>();
-    public User(string login, string password)
-    {
-        this.Login = login;
-        this.Password = password;
-      
-    }
-    public string Login { get; private set; }
-    public string Password { get; private set; }
 
-    public int Result
-    {
-        get 
-        {
-            return this.score.Sum();
-        }
-    }
-
-    public void AddScore(int number)
-    {
-        this.score.Add(number);
-    }
-}
