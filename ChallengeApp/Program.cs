5e671247befa75794dc3f2c41e90ea7ae2e992fc
Kamil Motulewicz <kamil.motulewicz@gmail.com>
2023-06-25 18:05:59 +0200

    l

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,14 +1,19 @@
 ﻿using System.Reflection.Metadata;
 
-User user1 = new User();
-User user2 = new User();
-User user3 = new User();
-User user4 = new User();
+User user1 = new User("Ewa","aaa");
+User user2 = new User("Tomek","ppp");
+User user3 = new User("Kamil","123");
+User user4 = new User("Dorota","ooo");
 
-user1.login = "Wojtek";
 class User
 {
-    public string login;
-    public string password;
-    private string username;
+  
+    public User(string login, string password)
+    {
+        this.Login = login;
+        this.Password = password;
+      
+    }
+    public string Login { get; private set; }
+    public string Password { get; private set; }
 }
