c2553967e2af844f6b245f35792585ab36efd1c7
Kamil Motulewicz <kamil.motulewicz@gmail.com>
2023-07-07 18:30:43 +0200

    Dzień 8 zadanie domowe

---
+++ ChallengeApp.Tests/TypeTest.cs
@@ -0,0 +1,54 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void CheckIfTwoEmplyeesAreDifferentUsers()
+        {
+            //Arrange
+            var employee1 = GetEmployee("Jan", "Kowalski","60");
+            var employee2 = GetEmployee("Jan", "Kowalski", "60");
+
+            //Assert
+            Assert.AreNotEqual(employee1, employee2);
+
+        }
+        [Test]
+        public void CheckIfDoubleValuesAreNotEqual()
+        {
+            //Arrange
+            var value1 = 1.231;
+            var value2 = 2.145;
+
+            //Assert
+            Assert.AreNotEqual(value1, value2);
+
+        }
+        [Test]
+        public void CheckIfStringValuesAreEqual()
+        {
+            //Arrange
+            var value1 = 1;
+            var value2 = 1;
+
+            //Assert
+            Assert.AreEqual(value1, value2);
+
+        }
+        [Test]
+        public void CheckIfStringsNameAreEqual()
+        {
+            //Arrange
+            var name1 = "Ewa";
+            var name2 = "Ewa";
+
+            //Assert
+            Assert.AreEqual(name1, name2);
+
+        }
+        private Employee GetEmployee(string name, string surname, string age)
+        {
+            return new Employee(name, surname, age) ;
+        }
+    }
+}
